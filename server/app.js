05504662311d453b596daecbc16c432e36dbf7e4
Ying Wang <ying29.wang@outlook.com>
2025-03-22 09:51:13 -0700

    create db.js and .env; update app.js

--- server/app.js
+++ server/app.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./db');
+const path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var app = express();
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
-app.use(logger('dev'));
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Default route
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to Camp Explorer API' });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
