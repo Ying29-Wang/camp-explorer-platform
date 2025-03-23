e19817be90cfc14c99a570d97721d99856f20d35
DominicEji <dominic.ejiogu@gmail.com>
2025-03-22 21:41:59 -0700

    Add routing with BrowserRouter for App component

---
+++ client/src/routes.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Add more routes here */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
