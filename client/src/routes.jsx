2240c31ddfc8843cb5ccaa0a0adf9295c11b458e
DominicEji <dominic.ejiogu@gmail.com>
2025-03-31 13:31:09 -0700

    I configured BrowserRouter to manage application routinga and defined routes for home, search results, authentication, profile, and camp details pages.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import SearchResults from './pages/SearchResults'; // Ensure this path is correct
+import SearchResults from './pages/SearchResults';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import ProfilePage from './pages/ProfilePage';
+import CampDetailsPage from './pages/CampDetailsPage';
 
 const AppRoutes = () => {
     return (
@@ -9,7 +13,10 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/search" element={<SearchResults />} />
-                {/* Add more routes here */}
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/camps/:id" element={<CampDetailsPage />} />
             </Routes>
         </Router>
     );
