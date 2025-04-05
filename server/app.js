241dc56b9ed5760643bd871cd681bdc4c3c41fa1
Ying Wang <ying29.wang@outlook.com>
2025-04-05 14:54:00 -0700

    update cors to exactly my cllient url

--- server/app.js
+++ server/app.js
@@ -21,28 +21,10 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-// Configure CORS based on environment
-// const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173'];
-// console.log('Allowed CORS origins:', allowedOrigins);
-
-// app.use(cors({
-//     origin: function(origin, callback) {
-//         // Allow requests with no origin (like mobile apps or curl requests)
-//         if (!origin) return callback(null, true);
-        
-//         if (allowedOrigins.indexOf(origin) === -1) {
-//             var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     },
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-
+// CORS middleware
 app.use(cors({
-    origin: '*',
+    origin: 'https://camp-explorer-l03o.onrender.com',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
