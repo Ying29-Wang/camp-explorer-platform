e879bf04ef9c2a812c99f2e87b08d37f8f7ce6e0
Ying Wang <ying29.wang@outlook.com>
2025-04-05 12:18:09 -0700

    edit the CORS configuration to use environment variables

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ connectDB();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
