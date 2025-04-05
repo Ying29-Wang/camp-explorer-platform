2deef56e9d825e9d987ec40e8f905975d72565f4
Ying Wang <ying29.wang@outlook.com>
2025-04-05 13:08:50 -0700

    Handle CORS

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ connectDB();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: ['https://camp-explorer-l03o.onrender.com', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
